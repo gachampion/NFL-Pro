2221adb340c0adbfb65494a144b1c078221f2669
Sahil Patel <patelsahil395@gmail.com>
2021-07-30 13:52:11 -0400

    added navigation buttons
    Todo: active state isn't being set

--- nfl-pro-temp/index.js
+++ nfl-pro-temp/index.js
@@ -53,3 +53,13 @@ $( ".lb7" ).click(function() {
     $( ".lb7" ).html('<p>OK</p>');
     $(".progressPercent3").css('width', '50%');
 });
+
+var header = document.getElementById("navContainer");
+var btns = header.getElementsByClassName("circle");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
