b10b3a02df3d88bb758295c649645849845c224a
Sahil Patel <patelsahil395@gmail.com>
2021-08-04 16:22:05 -0400

    Adjusted betting window and added queue window
    Todo: adjust styling to match each of setup pages

--- pro-setup/index.js
+++ pro-setup/index.js
@@ -59,7 +59,7 @@ $( ".lb8" ).click(function() {
     $( ".lb8" ).css('background-color', '#0C2131');
     $( ".lb8" ).css('box-shadow', 'inset 0px 4px 4px rgba(0, 0, 0, 0.25)');
     $( ".lb8" ).html('<p>OK</p>');
-    $(".progressPercent").css('width', '50%');
+    $(".progressPercent").css('width', '33%');
 });
 
 $( ".lb9" ).click(function() {
@@ -67,7 +67,31 @@ $( ".lb9" ).click(function() {
     $( ".lb9" ).css('background-color', '#0C2131');
     $( ".lb9" ).css('box-shadow', 'inset 0px 4px 4px rgba(0, 0, 0, 0.25)');
     $( ".lb9" ).html('<p>OK</p>');
-    $(".progressPercent").css('width', '50%');
+    $(".progressPercent").css('width', '66%');
+});
+
+$( ".lb10" ).click(function() {
+    $( ".lb10" ).css('color', '#00EE5F');
+    $( ".lb10" ).css('background-color', '#0C2131');
+    $( ".lb10" ).css('box-shadow', 'inset 0px 4px 4px rgba(0, 0, 0, 0.25)');
+    $( ".lb10" ).html('<p>OK</p>');
+    $(".progressPercent").css('width', '100%');
+});
+
+$( ".lb11" ).click(function() {
+    $( ".lb11" ).css('color', '#00EE5F');
+    $( ".lb11" ).css('background-color', '#0C2131');
+    $( ".lb11" ).css('box-shadow', 'inset 0px 4px 4px rgba(0, 0, 0, 0.25)');
+    $( ".lb11" ).html('<p>OK</p>');
+    $(".progressPercent2").css('width', '50%');
+});
+
+$( ".lb12" ).click(function() {
+    $( ".lb12" ).css('color', '#00EE5F');
+    $( ".lb12" ).css('background-color', '#0C2131');
+    $( ".lb12" ).css('box-shadow', 'inset 0px 4px 4px rgba(0, 0, 0, 0.25)');
+    $( ".lb12" ).html('<p>OK</p>');
+    $(".progressPercent2").css('width', '100%');
 });
 
 var header = document.getElementById("navContainer");
